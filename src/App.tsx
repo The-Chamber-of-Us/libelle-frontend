1802b74018577eb0dabba94778045f31726f5a96
Facundo Quiroga <facu.quirogaga@gmail.com>
2025-09-22 08:49:36 -0500

    feat: add landing page template with CTA to volunteer form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import VolunteerFormPage from "./pages/VolunteerFormPage";
 
 function App() {
-
-
   return (
-    <>
-      Hello World
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/volunteer" element={<VolunteerFormPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/VolunteerFormPage.tsx
@@ -0,0 +1,10 @@
+export default function VolunteerFormPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-semibold mb-4">Volunteer Form</h1>
+      <p className="text-gray-600">
+        The form will be implemented here soon.
+      </p>
+    </div>
+  );
+}
